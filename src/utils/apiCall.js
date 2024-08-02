bbc3845cf6c105da97bc0a748ae39cfc1718b7f5
vishalghag <vishalghag47@gmail.com>
2024-08-03 00:29:21 +0530

    first part done

--- src/utils/apiCall.js
+++ src/utils/apiCall.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_URL2 = process.env.REACT_APP_API_URL;
+// const API_URL2 = process.env.REACT_APP_API_URL;
 
+const API_URL2 = "https://66a4fac95dc27a3c190a5579.mockapi.io/products";
 export const fetchProducts = async () => {
   try {
     const response = await axios.get(API_URL2);
